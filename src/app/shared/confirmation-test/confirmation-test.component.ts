f5883d9bbcda624c87914b15f3dc8d713e2edc1b
m-ashraf-elfadli <mohamed.elfadli@2b.com.eg>
2026-01-21 01:08:53 +0200

    test dynamic dialog

--- src/app/shared/confirmation-test/confirmation-test.component.ts
+++ src/app/shared/confirmation-test/confirmation-test.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { ConfirmDialogComponent } from '../components/confirm-dialog/confirm-dialog.component';
 import { ConfirmationDialogConfig } from '../services/confirmation-popup.service';
+import { MessageService } from 'primeng/api';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { TestDialogComponent } from './test-dialog/test-dialog.component';
 
 @Component({
   selector: 'app-confirmation-test',
   standalone: true,
   imports: [ButtonModule, ConfirmDialogComponent],
   templateUrl: './confirmation-test.component.html',
-  styleUrl: './confirmation-test.component.scss'
+  styleUrl: './confirmation-test.component.scss',
+  providers: [DialogService, MessageService]
 })
 export class ConfirmationTestComponent {
   visible = false;
@@ -42,4 +46,29 @@ export class ConfirmationTestComponent {
   onCancel() {
     console.log('CANCELLED');
   }
+
+   ref: DynamicDialogRef | undefined;
+
+    constructor(public dialogService: DialogService, public messageService: MessageService) {}
+
+    show() {
+        this.ref = this.dialogService.open(TestDialogComponent, {
+            header: '',
+            width: '70%',
+            data: {
+                id: '51gF3'
+            },
+            contentStyle: { overflow: 'auto' },
+            baseZIndex: 10000,
+            maximizable: false,
+            modal: true
+        });
+
+        this.ref.onClose.subscribe((product: any) => {
+            if (product) {
+              console.log('Product selected:', product);
+                // this.messageService.add({ severity: 'info', summary: 'Product Selected', detail: product.name });
+            }
+        });
+    }
 }
