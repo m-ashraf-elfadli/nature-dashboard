0e6880eeb6baa7f0edf3468549a93bd2206ec4b5
ahmedyassin5821-sketch <ahmedyassin5821@gmail.com>
2026-01-20 12:16:23 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogComponent } from './shared/components/confirm-dialog/confirm-dialog.component';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [DialogService]
 })
 export class AppComponent {
   title = 'nature-dashboard';
+  ref: DynamicDialogRef | undefined;
+
+    constructor(public dialogService: DialogService) {}
+
+    show() {
+        this.ref = this.dialogService.open(ConfirmDialogComponent, { 
+          header: 'Select a Product',
+          modal:true,
+        });
+    }
 }
