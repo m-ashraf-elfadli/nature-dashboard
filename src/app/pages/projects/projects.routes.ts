cd7274ea9060c3bdbbbfb3f8d755c4783c4e107d
m-ashraf-elfadli <mohamed.elfadli@2b.com.eg>
2026-01-21 18:24:48 +0200

    test 2

--- src/app/pages/projects/projects.routes.ts
+++ src/app/pages/projects/projects.routes.ts
@@ -10,7 +10,7 @@ export const projectsRoutes: Routes = [
     path: 'add',
     loadComponent: () =>
       import('./project-form/project-form.component').then(
-        (m) => m.ProjectFormComponent
+        (m) => m.ProjectFormComponent 
       ),
     data: {
       title: 'Add New Project',
