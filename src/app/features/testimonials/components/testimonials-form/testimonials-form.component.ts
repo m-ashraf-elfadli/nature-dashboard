4a5bb6b55649f8a775bb2b07bb924b9f3d7642dc
ahmedyassin5821-sketch <ahmedyassin5821@gmail.com>
2026-01-26 13:03:57 +0200

    createion of testimonials

---
+++ src/app/features/testimonials/components/testimonials-form/testimonials-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonials-form',
+  imports: [],
+  templateUrl: './testimonials-form.component.html',
+  styleUrl: './testimonials-form.component.scss'
+})
+export class TestimonialsFormComponent {
+
+}
