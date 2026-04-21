4f5b9778fe5416f57a9436e4b15c13ae3cca4816
EzzAly74 <127905343+EzzAly74@users.noreply.github.com>
2026-04-21 13:56:25 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  enableBlogs: true,
+  enableBlogs: false,
   baseUrl: 'https://developer.neas.ae/api',
   mediaUrl: 'https://developer.neas.ae/api/media/',
 };
